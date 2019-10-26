afcfdab1b7256ffd0cde1ec5cb9f57a1d29dccb1
Phillip Gajadhar <phillipgajadhar@outlook.com>
2019-10-26 13:00:17 -0400

    Add insertion sorting algorithm

--- 04-sorting/main.c
+++ 04-sorting/main.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 void swap(void *, void *, int);
-void bubbleSort(int[], int);
+bool greaterThan(int, int);
+bool lessThan(int, int);
+void bubbleSort(int[], int, bool (*)(int, int));
+void insertionSort(int[], int);
 
 int main(int argc, char const *argv[])
 {
-  int n = 100000;
+  int n = 10;
   int numbers[n];
 
-  for (int i = n - 1; i >= 0; i--)
+  for (int i = 0; i < n; i++)
     numbers[i] = i;
 
-  // perform bubblesort
-  bubbleSort(numbers, n);
+  // perform sorting
+  // bubbleSort(numbers, n, lessThan);
+  // insertionSort(numbers, n);
 
   for (int i = 0; i < n; i++)
     printf("%d\n", numbers[i]);
@@ -34,10 +39,34 @@ void swap(void *x, void *y, int size_t)
   free(temp);
 }
 
-void bubbleSort(int numbers[], int n)
+bool greaterThan(int x, int y)
+{
+  return x > y;
+}
+
+bool lessThan(int x, int y)
+{
+  return !greaterThan(x, y);
+}
+
+void bubbleSort(int numbers[], int n, bool (*compare)(int x, int y))
 {
   for (int i = 0; i < n; i++)
     for (int j = 0; j < n - i - 1; j++)
-      if (numbers[j] > numbers[j + 1])
+      if ((*compare)(numbers[j], numbers[j + 1]))
         swap(&numbers[j], &numbers[j + 1], sizeof(int));
+}
+
+void insertionSort(int numbers[], int n)
+{
+  for (int i = 0; i < n; i++)
+  {
+    int j;
+    int insertee = numbers[i];
+
+    for (j = i; j > 0 && !(insertee > numbers[j - 1]); j--)
+      swap(&numbers[j - 1], &numbers[j], sizeof(int));
+
+    numbers[j] = insertee;
+  }
 }
